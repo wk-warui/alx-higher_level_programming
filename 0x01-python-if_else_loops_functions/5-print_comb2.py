97db92a56f441be9a2dabd8e93a60e1699d765cf
wk-warui <demarazeandrena@gmail.com>
2022-11-29 12:19:53 -0800

    a program that prints all numbers from 0 to 98 in decimal and in hexadecimal (as in the following example)

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for numbers in range(0, 100):
-    if numbers == 99:
-        print("{}".format(numbers))
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
     else:
-        print("{:02}".format(numbers), end=", ")
+        print("{:02}".format(number), end=", ")
