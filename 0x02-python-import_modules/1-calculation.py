519aaeece3e135303afcf11cde71f7c3fbdf9d83
wk-warui <demarazeandrena@gmail.com>
2022-12-01 02:38:33 -0800

    a program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,13 +2,12 @@
 
 if __name__ == "__main__":
 
-    """import functions from file"""
     from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
-print("{} + {} = {}".format(a, b, add(a, b))) """addition function"""
-print("{} - {} = {}".format(a, b, sub(a, b))) """subtraction function"""
-print("{} * {} = {}".format(a, b, mul(a, b))) """multiplication function"""
-print("{} / {} = {}".format(a, b, div(a, b))) """division function"""
 
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
