e768e239c00a36f41e6e6d8145a27660e4923446
wk-warui <demarazeandrena@gmail.com>
2022-12-05 03:29:51 -0800

    prints a matrix of integers.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/pyhton3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for col in matrix:
-            print("{:d}".format(col), end=" ")
-            if col != row[-1]:
-                else:
-                    print()
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
