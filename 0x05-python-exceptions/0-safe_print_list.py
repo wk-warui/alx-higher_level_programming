1d53b199b37a48dd26f8b47bcec0d85b96f07e0b
wk-warui <demarazeandrena@gmail.com>
2022-12-19 06:27:46 -0800

    IndexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    n = 0
+    i = 0
     if x == 0:
         print()
-        return n
+        return i
     try:
-        for n in range(x):
-            print(my_list[n], end="")
-            print()
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
     except IndexError:
         print()
-        return n
-    return n + 1
+        return i
+    return i + 1
