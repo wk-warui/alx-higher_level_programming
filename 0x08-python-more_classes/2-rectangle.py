2b58cd826cd7cd47ba007840a0706d0b5a7942d1
wk-warui <demarazeandrena@gmail.com>
2023-01-03 09:47:24 -0800

    Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,48 +5,47 @@
 class Rectangle:
     """Represent a rectangle"""
 
-    def __init__(self, width=0 height=0):
-        """Initialize a new rectangle.
-
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
         Args:
-            width (int): width of the new rectangle
-            height (int): height of the new rectangle
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get the width of the rectangle"""
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """Get the height of the rectangle"""
+        """Get/set the height of the Rectangle."""
         return self.__height
 
-    @width.setter
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """Get the area of the rectangle(height * width)"""
+        """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """Get the perimeter of the rectangle((height * 2) + (width * 2))"""
-        if self.__width == 0 or self.height == 0:
+        """Return the perimeter of the Rectangle."""
+        if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
