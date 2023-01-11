03bdc5394d0dfd10a51f51bc4c6e80f611769e28
wk-warui <demarazeandrena@gmail.com>
2023-01-11 13:30:26 -0800

    Python - Inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines an abject attribute lookup function"""
+
+
 def lookup(obj):
     """ returns a list of possible attributes """
-    return dir(obj)
+    return (dir(obj))
